f089fb3c8ca9207e40bcf540431067171f64c799
manavnotop <112702636+manavnotop@users.noreply.github.com>
2023-07-27 23:10:07 +0530

    Time: 8 ms (94.47%), Space: 16.1 MB (5.34%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -5,13 +5,16 @@ public:
         map<int, int> mpp;
         for(int i = 0; i < nums.size(); i++){
             mpp[nums[i]]++;
-        }
-
-        for( auto i: mpp){
-            if(i.second > (nums.size() / 3)){
-                ans.push_back(i.first);
+            if(mpp[nums[i]] == nums.size()/3 + 1){
+                ans.push_back(nums[i]);
             }
         }
+
+        // for( auto i: mpp){
+        //     if(i.second > (nums.size() / 3)){
+        //         ans.push_back(i.first);
+        //     }
+        // }
         return ans;
     }
 };
